e1e38ccde96f077497799a46aa14c7682c8033e4
JACI-LABPC <jaclynsaunders@gmail.com>
2020-09-19 14:13:33 -0700

    Changed to a simple exact string match
    Changed query to an exact string match query for peptide sequences. Significantly faster search speeds than Levenshtein-based search.

--- lib/proteomics/scripts/query_by_sequence.py
+++ lib/proteomics/scripts/query_by_sequence.py
@@ -1,17 +1,18 @@
 """
 name: query_by_sequence.py
 
-usage: query_by_sequence.py [--max-distance=0] sequence_file 
+usage: query_by_sequence.py sequence_file 
 
 commissioned by : Dr. Makoto Saito, 2013-03
 
 authorship: adorsk, 2013-05
+updated: Jaclyn Saunders, 2020-09
 
 description: This script queries a peptides database for the given set of
 peptide sequences.
 
 Outputs: a CSV document to stdout whose rows contains:
-    query_sequence | taxon_id | levenshtein_distance | match_sequence
+    query_sequence | taxon_id | match_sequence | count_in_taxon
 """
 
 """
@@ -30,10 +31,6 @@ Process arguments.
 """
 argparser = argparse.ArgumentParser(description=(
     'Query database for peptide sequences'))
-argparser.add_argument('--max-distance', type=int, nargs='?', default=0,
-                       help=(
-                           'List of FASTA files containing protein sequences.'
-                       ))
 argparser.add_argument('--sequence-file', help=(
     'File containing one amino acid sequence per line.'))
 
@@ -44,33 +41,13 @@ Main method.
 """
 def main():
     args = argparser.parse_args()
+    print args
 
     logger = logging.getLogger('query_by_sequence')
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.INFO)
 
-    # Define levenshtein function in SQLite.
-    try:
-        def levenshtein(s1,s2):
-            l1 = len(s1)
-            l2 = len(s2)
-            matrix = [range(l1 + 1)] * (l2 + 1)
-            for zz in range(l2 + 1):
-              matrix[zz] = range(zz,zz + l1 + 1)
-            for zz in range(0,l2):
-              for sz in range(0,l1):
-                if s1[sz] == s2[zz]:
-                  matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz])
-                else:
-                  matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz] + 1)
-            return matrix[l2][l1]
-
-        connection = db.get_connection()
-        connection.connection.create_function("LEVENSHTEIN", 2, levenshtein)
-    except Exception as e:
-        logger.exception('Could not define Levenshtein distance function: %s' % e)
-        raise e
-
+    connection = db.get_connection()
     session = db.get_session(bind=connection)
 
     # Read in sequences to query.
@@ -86,21 +63,21 @@ def main():
                         "or a set of sequences via the --sequence-file option")
 
     # Print headers. 
-    headers = ['query', 'taxon', 'lev_distance', 'match']
+    headers = ['query', 'taxon', 'match', 'count']
     print ','.join(headers)
 
     # Execute query for each sequence and print results.
     for seq in sequences:
-        lev_dist = func.LEVENSHTEIN(Peptide.sequence, seq)
-        q = (session.query(TaxonDigest.taxon_id, lev_dist,
-                           Peptide.sequence)
+        q = (session.query(TaxonDigest.taxon_id, 
+                           Peptide.sequence, TaxonDigestPeptide.count)
              .select_from(Peptide)
              .join(TaxonDigestPeptide)
              .join(TaxonDigest)
-             .filter(lev_dist <= args.max_distance)
+             .filter(Peptide.sequence == seq)
             )
         for row in q:
             print ','.join([str(s) for s in [seq] + list(row)])
 
 if __name__ == '__main__':
     main()
+
